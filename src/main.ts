a9df53a41a0c3fad9fc2dbed139d9f373723dccf
Toomas Tahves <toomastahves@hotmail.com>
2020-10-25 19:36:22 +0200

    feat: add ssl

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { readFileSync } from 'fs';
 
 dotenv.config();
 
+const httpsOptions = {
+  key: readFileSync('./ssl/server.key'),
+  cert: readFileSync('./ssl/public-certificate.pem'),
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: true }));
   await app.listen(process.env.PORT);
